250aaa56caae28a4554a90b9c4a1c7b9072952e6
Thanawat <thanawatk65@nu.ac.th>
2024-10-10 22:49:24 +0700

    edit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,94 @@
-import 'package:account/screens/form_screen.dart';
-import 'package:account/screens/home_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:account/provider/transaction_provider.dart';
+import 'package:account/screens/edit_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) {
-          return TransactionProvider();
-        }),
-      ],
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: const Text("รายชื่อ Platform"),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.exit_to_app),
+              onPressed: () {
+                SystemNavigator.pop();
+              },
+            ),
+          ],
         ),
-        home: const MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Provider.of<TransactionProvider>(context, listen: false).initData();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          body: TabBarView(
-            children: [
-              HomeScreen(),
-              FormScreen(),
-            ],
-          ),
-          bottomNavigationBar: TabBar(
-            tabs: [
-              Tab(text: "รายการธุรกรรม", icon: Icon(Icons.list),),
-              Tab(text: "เพิ่มข้อมูล", icon: Icon(Icons.add),),
-            ],
-          ),
-        ));
+        body: Consumer(
+          builder: (context, TransactionProvider provider, Widget? child) {
+            if (provider.transactions.isEmpty) {
+              return const Center(
+                child: Text('ไม่มีรายการ'),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: provider.transactions.length,
+                itemBuilder: (context, index) {
+                  var statement = provider.transactions[index];
+                  return Card(
+                    elevation: 5,
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                    child: ListTile(
+                      title: Text(statement.pName),
+                      subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('ชื่อ Platform: ${statement.pName}' , style: TextStyle(fontSize: 16)),
+                        Text('ชื่อผู้ก่อตั้ง: ${statement.fName}', style: TextStyle(fontSize: 16)),
+                        Text('วันที่เปิดให้บริการ: ${statement.pDate}', style: TextStyle(fontSize: 16)),
+                        Text('จำนวนผู้ใช้งาน: ${statement.userAmount}', style: TextStyle(fontSize: 16)),
+                  
+                        Text(
+                          DateFormat('dd MMM yyyy hh:mm:ss').format(statement.date),
+                          style: TextStyle(fontSize: 14, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: FittedBox(
+                          child: Text('${statement.pName[0].toUpperCase()}'),
+                        ),
+                      ),
+                      trailing: IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          provider.deleteTransaction(statement.keyID);
+                        },
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return EditScreen(statement: statement);
+                            },
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                },
+              );
+            }
+          },
+        )
+        // This trailing comma makes auto-formatting nicer for build methods.
+        );
   }
 }
